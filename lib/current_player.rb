757fd3b076ba52ef5ff01df1359a7fa868131082
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-08 22:28:50 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,8 +9,9 @@ def turn_count(board)
   return counter
 end
 
-def current_player(something)
-  if something % 2 != 0
+def current_player(board)
+  number_turn = turn_count(board)
+  if number_turn % 2 != 0
     return 'X'
   else
     return 'O'
