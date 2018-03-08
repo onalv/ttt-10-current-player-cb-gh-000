b8ed9735cea8c2ab18ba50d26ad06393efbf9011
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-08 22:21:33 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,11 @@
-def turn_count
-
+def turn_count(board)
+  counter = 0
+  board.each do |place|
+    if place == 'X' or place == 'O'
+      counter += 1
+    end
+    return counter
+  end
 end
 
 def current_player
